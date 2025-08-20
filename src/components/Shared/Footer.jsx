eca0ddb5e10278625e64eaf8dfe8e01517d34e0c
tm-fuad05 <tm.fuad05@gmail.com>
2025-08-20 19:57:17 +0600

    footer done

---
+++ src/components/Shared/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="bg-primary py-5 ">
+      <p className="text-black text-center font-medium">
+        All rights reserved © 2025 Farhatul Islam. Privacy Policy | Terms of
+        Service
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Shared/Navbar";
+import Footer from "../components/Shared/Footer";
 
 const Root = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer />
     </div>
   );
 };
