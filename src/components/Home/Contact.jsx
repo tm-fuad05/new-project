37265605e884b0e9b591658fecb012bc2e78120d
tm-fuad05 <tm.fuad05@gmail.com>
2025-08-20 20:44:23 +0600

    input size

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -90,13 +90,15 @@ const Contact = () => {
                 name="email"
                 id="email"
                 placeholder="Enter your email to get started"
-                className="w-2/3 peer border-[#e5eaf2]  dark:placeholder:text-slate-500 dark:border-slate-600 border rounded-full outline-none text-xs pl-12 pl-10 pr-4 py-3 w-full focus:border-[#3B9DF8] transition-colors duration-300"
+                className=" border-[#e5eaf2]  w-[60%] dark:placeholder:text-slate-500 dark:border-slate-600 border rounded-full outline-none text-xs pl-12 pl-10 pr-4 py-3 w-full focus:border-[#3B9DF8] transition-colors duration-300"
               />
-              <ArrowButton
-                btnName={"Get Started"}
-                arrowColor={"white"}
-                bgColor={"primary"}
-              />
+              <div>
+                <ArrowButton
+                  btnName={"Get Started"}
+                  arrowColor={"white"}
+                  bgColor={"primary"}
+                />
+              </div>
             </div>
           </form>
           {/* Contact Number */}
