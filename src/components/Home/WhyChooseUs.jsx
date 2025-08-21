c03fbfb09a1faf200c75640252c598d6b1e81d4f
tm-fuad05 <tm.fuad05@gmail.com>
2025-08-22 01:55:44 +0600

    why choose us lines added

--- src/components/Home/WhyChooseUs.jsx
+++ src/components/Home/WhyChooseUs.jsx
@@ -6,6 +6,10 @@ import num3 from "../../assets/numbers/num3.png";
 import num4 from "../../assets/numbers/num4.png";
 import Grid from "../Shared/Grid";
 import Horizontal from "../Shared/Horizontal";
+import toplong from "../../assets/Lines/top-long.png";
+import topshort from "../../assets/Lines/top-short.png";
+import bottomlong from "../../assets/Lines/bottom-long.png";
+import bottomshort from "../../assets/Lines/bottom-short.png";
 
 const WhyChooseUs = () => {
   const cards = [
@@ -40,8 +44,8 @@ const WhyChooseUs = () => {
   ];
 
   return (
-    <div className="flex flex-col gap-8 my- pt-10 pb-20 min-h-screen relative overflow-hidden">
-      <Horizontal width={"7/12"} />
+    <div className="flex flex-col gap-8 my-20 pt-10 pb-36 min-h-screen relative overflow-hidden">
+      <Horizontal width={"7/12"} top={"0"} />
       {/*top corner Blurry lights */}
 
       <div className="absolute z-10 blur-2xl opacity-15 top-[250px] -left-[200px] w-[300px] h-[300px] rounded-full bg-primary animate-pulse-slow" />
@@ -65,8 +69,30 @@ const WhyChooseUs = () => {
         </p>
       </div>
       {/* Cards */}
-      <div className="flex gap-5 px-20 z-30">
-        <figure className="w-full h-[600px] card-gradient-border relative z-30">
+      <div className="grid grid-cols-5 gap-16 px-20 z-30 mt-20 relative">
+        {/* Lines */}
+        <img
+          src={toplong}
+          alt=""
+          className="absolute left-1/2 -translate-x-1/2  -top-[85px]"
+        />
+        <img
+          src={topshort}
+          alt=""
+          className="absolute left-[calc(50%-15px)] -translate-x-1/2  -top-[30px] "
+        />
+        <img
+          src={bottomlong}
+          alt=""
+          className="absolute left-1/2 -translate-x-1/2 -bottom-[85px] "
+        />
+        <img
+          src={bottomshort}
+          alt=""
+          className="absolute left-[calc(50%-15px)]  -translate-x-1/2 -bottom-[30px] "
+        />
+
+        <figure className="col-span-2 card-gradient-border relative z-30 scale-[118%]">
           <div className="absolute bottom-0 left-0 p-8 bg-black/30 rounded-2xl inset-0 flex flex-col gap-5 z-10"></div>
           <img
             src={tablet}
@@ -74,7 +100,7 @@ const WhyChooseUs = () => {
             className="w-full h-full object-cover rounded-[20px]"
           />
         </figure>
-        <div className="col-span-2 grid grid-cols-2 gap-5">
+        <div className="col-span-3 grid grid-cols-2 gap-5">
           {cards.map((c) => (
             <div className="bg-white rounded-2xl p-5 flex flex-col gap-5 group hover:bg-primary duration-300">
               <img src={c.number} alt="" className="w-12 h-12 object-contain" />
@@ -87,6 +113,8 @@ const WhyChooseUs = () => {
           ))}
         </div>
       </div>
+
+      <Horizontal width={"7/12"} bottom={"0"} />
     </div>
   );
 };
--- src/components/Shared/Horizontal.jsx
+++ src/components/Shared/Horizontal.jsx
@@ -1,7 +1,7 @@
-const Horizontal = ({ width }) => {
+const Horizontal = ({ width, top, bottom }) => {
   return (
     <div
-      className={`w-${width} h-[1px] bg-gradient-to-r from-transparent via-primary to-transparent absolute z-30 left-1/2 -translate-x-1/2 top-0`}
+      className={`w-${width} h-[1px] bg-gradient-to-r from-transparent via-primary to-transparent absolute z-30 left-1/2 -translate-x-1/2 top-${top} bottom-${bottom}`}
     />
   );
 };
